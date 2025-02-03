dd27d53da48744b365aae6fe68155e095ba9a94c
Augusto Maldonado <augusto.maldonado@openk.com.br>
2025-02-03 18:38:02 -0300

    refactor: movendo o tailwind de volta para a v3

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#3294F8",
+        title: "#E7EDF4",
+        subtitle: "#C4D4E3",
+        text: "#AFC2D4",
+        span: "#7B96B2",
+        label: "#3A536B",
+        border: "#1C2F41",
+        post: "#112131",
+        profile: "#0B1B2B",
+        background: "#071422",
+        input: "#040F1A",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
